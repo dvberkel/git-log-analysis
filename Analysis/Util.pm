aec79c0c72e565b418fb0070ec08dc45a44ef110
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-02-21 15:13:22 +0100

    Log Analysis created
    The analysis report is created.

--- Analysis/Util.pm
+++ Analysis/Util.pm
@@ -8,10 +8,25 @@ require Exporter;
 our $VERSION = '1.0.0';
 
 our @ISA = qw/Exporter/;
-our @EXPORT_OK = qw/analyse report/
+our @EXPORT_OK = qw/analyse report/;
 
 sub analyse {
+    my %analysis = ();
+    while(my $line = <>) {
+	if ($line =~ m/\[([^\]]+)\]/) {
+	    if (! exists $analysis{$1}) {
+		$analysis{$1} = 0;
+	    }
+	    $analysis{$1}++;
+	}
+    }
+    return %analysis;
 }
 
 sub report {
+    my $analysis = shift @_;
+    for my $key (keys %$analysis) {
+	my $value = $analysis->{$key};
+	print "$key : $value\n";
+    }
 }
---
+++ analyse.pl
@@ -0,0 +1,12 @@
+#! /usr/bin/env perl
+
+use strict;
+use warnings;
+
+use FindBin '$Bin';
+use lib "$Bin";
+
+use Analysis::Util qw/analyse report/;
+
+my %analysis = analyse();
+report(\%analysis)
