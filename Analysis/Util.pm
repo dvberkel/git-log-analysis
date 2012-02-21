b6d9ac925f708386f70cd5665f54018e11b064e4
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-02-21 14:31:32 +0100

    Skeleton package
    A skeleton package is added to the project. It will serve as a
    starting ground for development.

---
+++ Analysis/Util.pm
@@ -0,0 +1,17 @@
+package Analysis::Util;
+
+use strict;
+use warnings;
+
+require Exporter;
+
+our $VERSION = '1.0.0';
+
+our @ISA = qw/Exporter/;
+our @EXPORT_OK = qw/analyse report/
+
+sub analyse {
+}
+
+sub report {
+}
