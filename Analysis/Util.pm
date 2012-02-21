3a7f992086c307c09d25456c6a9b5bfca750a899
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-02-21 15:51:59 +0100

    [FIX] Default corrected
    The default keywords now counts total number of commits.

--- Analysis/Util.pm
+++ Analysis/Util.pm
@@ -11,16 +11,17 @@ our $VERSION = '1.0.0';
 our @ISA = qw/Exporter/;
 our @EXPORT_OK = qw/analyse report/;
 
-my $DEFAULT = "default";
+my $DEFAULT = "total commits";
 
 sub analyse {
     my %analysis = ();
     while(my $line = <>) {
-	my $key = $DEFAULT;
 	if ($line =~ m/\[([^\]]+)\]/) {
-	    $key = $1;
+	    increment(\%analysis, $1);
 	}
-	increment(\%analysis, $key);
+	if ($line =~ m/^commit/) {
+	    increment(\%analysis, $DEFAULT);
+	}
     }
     return %analysis;
 }
