5fcfd994b2cfde6c48b9df07ecc7025b38ce1c26
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-03-02 14:12:06 +0100

    [REFACTOR] Move code
    The code is moved to the util package.

--- Analysis/Util.pm
+++ Analysis/Util.pm
@@ -9,7 +9,7 @@ require Exporter;
 our $VERSION = '1.0.0';
 
 our @ISA = qw/Exporter/;
-our @EXPORT_OK = qw/analyse report/;
+our @EXPORT_OK = qw/analyse report analyseDir/;
 
 my $DEFAULT = "total commits";
 
@@ -50,4 +50,54 @@ sub report {
 sub padding {
     my ($length, $maximum) = @_;
     return "." x (5 + $maximum - $length);
+}
+
+
+sub analyseDir {
+    my ($directory) = @_;
+    if (usesGit($directory)) {
+	process($directory)
+    }
+}
+
+sub usesGit {
+    my ($dir) = @_;
+    return -e $dir && -d $dir && -d "$dir/.git";
+}
+
+sub process {
+    my ($directory) = @_;
+    pipe(READER, WRITER);
+    WRITER->autoflush(1);
+    if (my $pid = fork) {
+	parentProcess($pid, $directory, *READER, *WRITER);
+    } else {
+	childProcess($pid, $directory, *READER, *WRITER);
+    }
+}
+
+sub parentProcess {
+    my ($pid, $directory, $READER, $WRITER) = @_;
+    close $WRITER;
+    print "$directory\n";
+    print "=" x length($directory), "\n";
+    while (my $line = <$READER>) {
+	print $line;
+    }
+    close $READER;
+    print "\n";
+    waitpid($pid, 0);
+}
+
+sub childProcess {
+    my ($pid, $directory, $READER, $WRITER) = @_;
+    die "can not fork: $!\n" unless defined $pid;
+    close $READER;
+    chdir $directory;
+    open(GITLOG, "git log |");
+    my %analysis = analyse(*GITLOG);
+    print $WRITER report(\%analysis);
+    close(GITLOG);
+    close $WRITER;
+    exit(0);	    
 }
--- analyse-dirs.pl
+++ analyse-dirs.pl
@@ -8,57 +8,8 @@ use lib "$Bin";
 
 use IO::Handle;
 
-use Analysis::Util qw/analyse report/;
+use Analysis::Util qw/analyseDir/;
 
 foreach my $directory (@ARGV) {
     analyseDir($directory);
-}
-
-sub analyseDir {
-    my ($directory) = @_;
-    if (usesGit($directory)) {
-	process($directory)
-    }
-}
-
-sub usesGit {
-    my ($dir) = @_;
-    return -e $dir && -d $dir && -d "$dir/.git";
-}
-
-sub process {
-    my ($directory) = @_;
-    pipe(READER, WRITER);
-    WRITER->autoflush(1);
-    if (my $pid = fork) {
-	parentProcess($pid, $directory, *READER, *WRITER);
-    } else {
-	childProcess($pid, $directory, *READER, *WRITER);
-    }
-}
-
-sub parentProcess {
-    my ($pid, $directory, $READER, $WRITER) = @_;
-    close $WRITER;
-    print "$directory\n";
-    print "=" x length($directory), "\n";
-    while (my $line = <$READER>) {
-	print $line;
-    }
-    close $READER;
-    print "\n";
-    waitpid($pid, 0);
-}
-
-sub childProcess {
-    my ($pid, $directory, $READER, $WRITER) = @_;
-    die "can not fork: $!\n" unless defined $pid;
-    close $READER;
-    chdir $directory;
-    open(GITLOG, "git log |");
-    my %analysis = analyse(*GITLOG);
-    print $WRITER report(\%analysis);
-    close(GITLOG);
-    close $WRITER;
-    exit(0);	    
 }
