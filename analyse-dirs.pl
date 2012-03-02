b2f9e0b8971e8b954f8e08956c7f25167d5f6247
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-03-02 13:13:39 +0100

    [REFACTOR] Parent & Child
    The parent & child process are defined.

--- analyse-dirs.pl
+++ analyse-dirs.pl
@@ -12,27 +12,43 @@ use Analysis::Util qw/analyse report/;
 
 foreach my $directory (@ARGV) {
     if (usesGit($directory)) {
-	pipe(READER, WRITER);
-	WRITER->autoflush(1);
-	if (my $pid = fork) {
-	    close WRITER;
-	    while (my $line = <READER>) {
-		print $line;
-	    }
-	    close READER;
-	    waitpid($pid, 0);
-	} else {
-	    die "can not fork: $!\n" unless defined $pid;
-	    close READER;
-	    print WRITER "Sending: $directory\n";
-	    print WRITER "Finished sending\n";
-	    close WRITER;
-	    exit(0);	    
-	}
+	process($directory)
     }
 }
 
 sub usesGit {
     my ($dir) = @_;
     return -e $dir && -d $dir && -d "$dir/.git";
+}
+
+sub process {
+    my ($directory) = @_;
+    pipe(READER, WRITER);
+    WRITER->autoflush(1);
+    if (my $pid = fork) {
+	parentProcess($pid, *READER, *WRITER);
+    } else {
+	childProcess($pid, $directory, *READER, *WRITER);
+    }
+}
+
+sub parentProcess {
+    my ($pid, $READER, $WRITER) = @_;
+    close $WRITER;
+    while (my $line = <$READER>) {
+	print $line;
+    }
+    close $READER;
+    waitpid($pid, 0);
+
+}
+
+sub childProcess {
+    my ($pid, $directory, $READER, $WRITER) = @_;
+    die "can not fork: $!\n" unless defined $pid;
+    close $READER;
+    print $WRITER "Sending: $directory\n";
+    print $WRITER "Finished sending\n";
+    close $WRITER;
+    exit(0);	    
 }
